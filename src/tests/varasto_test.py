02392e620536f1602860f92314e3838bcc962978
Rano-9 <esamatti.karvonen@pispalanit.fi>
2024-10-31 15:57:57 +0200

    Varasto testien täydennys

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -4,6 +4,9 @@ from varasto import Varasto
 
 class TestVarasto(unittest.TestCase):
     def setUp(self):
+        self.varastoneg = Varasto(-10,-2)
+        self.varastoalku = Varasto(10,2)
+        self.varastotäys = Varasto(10,12)
         self.varasto = Varasto(10)
 
     def test_konstruktori_luo_tyhjan_varaston(self):
@@ -15,8 +18,13 @@ class TestVarasto(unittest.TestCase):
 
     def test_lisays_lisaa_saldoa(self):
         self.varasto.lisaa_varastoon(8)
+        
 
         self.assertAlmostEqual(self.varasto.saldo, 8)
+
+        #testataan varstoon lisääminen negatiivinen ja kun varasto on melko täynnä
+        self.varastoneg.lisaa_varastoon(-1)
+        self.varastoalku.lisaa_varastoon(10)
 
     def test_lisays_lisaa_pienentaa_vapaata_tilaa(self):
         self.varasto.lisaa_varastoon(8)
@@ -38,3 +46,12 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+        # testi varastosta otto negatiivinen luku
+        self.varastoneg.ota_varastosta(-2)
+
+        # testi ottetaan varastosta yli tilavuuden
+        self.varastotäys.ota_varastosta(12)
+    
+    def test_str_printti(self):
+        print(self.varasto)
