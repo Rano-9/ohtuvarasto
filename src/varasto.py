82d61d965af86ce969d73163abf4086528aeb79f
Rano-9 <esamatti.karvonen@pispalanit.fi>
2024-11-08 17:31:53 +0200

    viiko2 teht3

--- src/varasto.py
+++ src/varasto.py
@@ -16,7 +16,8 @@ class Varasto:
             # täyteen ja ylimäärä hukkaan!
             self.saldo = tilavuus
 
-    # huom: ominaisuus voidaan myös laskea. Ei tarvita erillistä kenttää viela_tilaa tms.
+# huom: ominaisuus voidaan myös laskea.
+# Ei tarvita erillistä kenttää viela_tilaa tms
     def paljonko_mahtuu(self):
         return self.tilavuus - self.saldo
 
@@ -34,11 +35,9 @@ class Varasto:
         if maara > self.saldo:
             kaikki_mita_voidaan = self.saldo
             self.saldo = 0.0
-
             return kaikki_mita_voidaan
 
         self.saldo = self.saldo - maara
-
         return maara
 
     def __str__(self):
